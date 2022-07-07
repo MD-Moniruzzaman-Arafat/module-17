0e2deb4a871e7cf6551fd050778e4dd4d9e99a0a
MD Moniruzzaman Arafat <arafatmonir091@gmail.com>
2022-07-07 22:22:02 +0600

    variable datatype typeof concatination parseint parsefloat tofixed

---
+++ app.js
@@ -0,0 +1,18 @@
+// var number = 10;
+// console.log(typeof number);
+// var number1 = 10;
+// var number2 = 30;
+// var result = number1 + number2
+// console.log(result)
+// number1 += 10
+// console.log(number1)
+// var name = 'arafat';
+// var name2 = 'rahman';
+// var fullName = name + ' ' + name2;
+// console.log(fullName);
+
+// var number = 10.65986;
+// var number = 10;
+// console.log(parseFloat(number))
+var number = Math.PI;
+console.log(number.toFixed(1))
